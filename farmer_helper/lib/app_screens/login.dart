09833a7507d392396a325f3b76a3d500459757b1
IsuruBRanapana <31366873+IsuruBRanapana@users.noreply.github.com>
2019-10-29 23:00:39 +0530

    firebase connected

---
+++ farmer_helper/lib/app_screens/login.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Login extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    return _LoginState();
+  }
+}
+
+class _LoginState extends State<Login>{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+
+}
