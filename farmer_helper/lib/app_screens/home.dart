a3b39e90a554fbb06cc4d6552181487e9c01fc6d
IsuruBRanapana <31366873+IsuruBRanapana@users.noreply.github.com>
2019-11-15 23:49:33 +0530

    Updated

---
+++ farmer_helper/lib/app_screens/home.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:farmer_helper/app_screens/home_tabs/h_allproblems.dart' as AllProblems;
+import 'package:farmer_helper/app_screens/home_tabs/h_latestproblems.dart' as LatestProblems;
+import 'package:farmer_helper/app_screens/home_tabs/accesscamera.dart' as AccessCamera;
+import 'package:farmer_helper/app_screens/home_tabs/h_filteredproblems.dart' as FilterProblems;
+import 'package:farmer_helper/app_screens/home_tabs/h_aboutapp.dart' as AboutApp;
+
+class MyTabs extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    return _MyTabsState();
+  }
+}
+class _MyTabsState extends State<MyTabs> with SingleTickerProviderStateMixin{
+
+  TabController controller;
+  @override
+  void initState(){
+    super.initState();
+    controller=TabController(vsync: this,length: 5);
+  }
+
+  @override
+  void dispose(){
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        title: Text(
+          'Bottom Navigation'
+        ),
+      ),
+      bottomNavigationBar: 
+      Material(
+        color: Colors.deepOrange,
+        child: TabBar(
+          controller: controller,
+          tabs: <Tab>[
+            Tab(
+              icon: Icon(Icons.access_alarms),
+            ),
+            Tab(
+              icon: Icon(Icons.backup),
+            ),
+            Tab(
+              icon: Icon(Icons.cake),
+            ),
+            Tab(
+              icon: Icon(Icons.dashboard),
+            ),
+            Tab(
+              icon: Icon(Icons.edit),
+            )
+          ],
+        ),
+      ),
+      body: TabBarView(
+        controller: controller,
+        children: <Widget>[
+          AllProblems.,
+          LatestProblems.,
+          AccessCamera.,
+          FilterProblems.,
+          AboutApp.,
+        ],
+      ),
+    );
+  }
+}
+
